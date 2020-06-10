69a024ddf5b0f122b16907ffea6e6d65433530f3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-06-10 20:09:56 -0300

    smallest-divisor, prime?, expmod, fermat-test, fast-prime? (wrong)

--- src/exercises/chapter1/text.clj
+++ src/exercises/chapter1/text.clj
@@ -202,3 +202,37 @@
 
 (gcd 206 40)
 ; => 2
+
+(defn divides? [a b] (= (rem b a) 0))
+
+(defn find-divisor [n test-divisor]
+  (cond (> (square test-divisor) n) n
+        (divides? test-divisor n) test-divisor
+        :else (find-divisor n (+ test-divisor 1))))
+
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+
+(defn prime? [n]
+  (= n (smallest-divisor n)))
+
+(filter prime? (range 1 20))
+; => (1 2 3 5 7 11 13 17 19)
+
+(defn expmod [base exp m]
+  (cond (= exp 0) 1
+        (even? exp) (rem (square (expmod base (/ exp 2) m)) m)
+        :else (rem (* base (expmod base (- exp 1) m)) m)))
+
+(defn fermat-test [n]
+  (defn try-it [a]
+    (= (expmod a n n) a))
+  (try-it (+ 1 (rand (- n 1)))))
+
+(defn fast-prime? [n times]
+  (cond (= times 0) true
+        (fermat-test n) (fast-prime? n (- times 1))
+        :else false))
+
+;(fast-prime? 1999 1000)
+;??????
