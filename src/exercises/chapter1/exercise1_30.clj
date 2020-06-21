d6cf708709b1053f259dc7b2331188b12486176c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-06-21 18:03:49 -0300

    Exercise 1.30

--- src/exercises/chapter1/exercise1_30.clj
+++ src/exercises/chapter1/exercise1_30.clj
@@ -1,1 +1,34 @@
-(ns exercises.chapter1.exercise1-30)
+(ns exercises.chapter1.exercise1_30)
+
+(defn sum [term a next b]
+  (defn iter [a result]
+    (if (> a b)
+      result
+      (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(defn sum-integers [a b]
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+; => 55
+
+(defn cube [x]
+  (* x x x))
+
+(defn sum-cubes [a b]
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+; => 3025
+
+(defn pi-sum [a b]
+  (defn pi-term [x]
+    (/ 1.0 (* x (+ x 2))))
+  (defn pi-next [x]
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+; => 3.139592655589782
+
