051e408b402367a92f797f966032a5a728108e72
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-08-14 14:50:58 -0300

    Exercise 2.3 - representing rectangle with points and exercising horizontal/vertical segments as abstractions barriers

--- src/exercises/chapter2/exercise2_3.clj
+++ src/exercises/chapter2/exercise2_3.clj
@@ -48,8 +48,6 @@
 (defn vertical-rectangle-segment [rectangle]
   (first (rest rectangle)))
 
-;I need an abstraction that comes from math
-
 (defn euclidean-distance [point-a point-b]
   (let [xa (x-point point-a)
         xb (x-point point-b)
@@ -59,18 +57,24 @@
     (sqrt (+ (square (- xb xa))
              (square (- yb ya))))))
 
-(euclidean-distance (make-point 3 10) (make-point 3 15))
+(def i (make-point 3 10))
+(def j (make-point 3 15))
+(def k (make-point 5 15))
+
+(euclidean-distance i j)
 ; => 5.000023178253949
 
-(euclidean-distance (make-point 3 15) (make-point 5 15))
+(euclidean-distance j k)
 ; => 2.0000000929222947
 
 (defn segment-length [segment]
   (euclidean-distance (start-segment segment) (end-segment segment)))
 
-(segment-length (make-segment (make-point 3 10) (make-point 3 15)))
+(segment-length (make-segment i j))
 ; => 5.000023178253949
 
+(segment-length (make-segment j k))
+; => 2.0000000929222947
 
 (defn rectangle-perimeter [rectangle]
   (+ (segment-length (horizontal-rectangle-segment rectangle))
@@ -89,6 +93,35 @@
 (def v (make-segment vs ve))
 
 (def r (make-rectangle h v))
+
+(rectangle-perimeter r)
+; a = 12, b=2 then p = 14
+; => 14.000000105330983
+
+(rectangle-area r)
+; a = 12, b=2 then a = 24
+; => 24.000001139884912
+
+(defn make-rectangle-with-points [horizontal-start-point horizontal-end-point vertical-start-point vertical-end-point]
+  (cons horizontal-start-point (cons horizontal-end-point (cons vertical-start-point (cons vertical-end-point '())))))
+
+(defn horizontal-rectangle-segment [rectangle]
+  (let [hs (first rectangle)
+        he (first (rest rectangle))]
+    (make-segment hs he)))
+
+(defn vertical-rectangle-segment [rectangle]
+  (let [vs (first (rest (rest rectangle)))
+        ve (first (rest (rest (rest rectangle))))]
+    (make-segment vs ve)))
+
+(def hs (make-point 3 3))
+(def he (make-point 15 3))
+
+(def vs (make-point 3 3))
+(def ve (make-point 3 5))
+
+(def r (make-rectangle-with-points hs he vs ve))
 
 (rectangle-perimeter r)
 ; a = 12, b=2 then p = 14
