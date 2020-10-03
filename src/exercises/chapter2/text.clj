af405b2484d1a90866e4a12d968a390768bb34af
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-10-03 15:31:27 -0300

    Mapping over trees

--- src/exercises/chapter2/text.clj
+++ src/exercises/chapter2/text.clj
@@ -314,3 +314,22 @@ one-through-four
 
 (count-leaves (list x x))
 ; => 8
+
+(defn scale-tree [tree factor]
+        (cond (not (seq? tree)) (* tree factor)
+              (empty? tree) nil
+              :else (cons (scale-tree (first tree) factor)
+                          (scale-tree (rest tree) factor))))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+; => (10 (20 (30 40) 50) (60 70))
+
+(defn scale-tree [tree factor]
+  (map (fn [sub-tree]
+         (if (seq? sub-tree)
+           (scale-tree sub-tree factor)
+           (* sub-tree factor)))
+       tree))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+; => (10 (20 (30 40) 50) (60 70))
