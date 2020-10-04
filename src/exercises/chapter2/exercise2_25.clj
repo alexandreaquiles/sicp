8d26215b923e2dc4d018620496ccd0586b3713e0
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-10-04 08:47:45 -0300

    fix Exercise 2.25

--- src/exercises/chapter2/exercise2_25.clj
+++ src/exercises/chapter2/exercise2_25.clj
@@ -5,14 +5,7 @@
 
 (def list1 '(1 3 (5 7) 9))
 (def list2 '((7)))
-(def list3 '(1 (2 (3 (4 (5 (6 7)))))Exercise 2.26: Suppose we define x and y to be two lists:
-(define x (list 1 2 3))
-(define y (list 4 5 6))
-What result is printed by the interpreter in response to eval-
-uating each of the following expressions:
-(append x y)
-(cons x y)
-(list x y)))
+(def list3 '(1 (2 (3 (4 (5 (6 7)))))))
 
 ; list1
 
