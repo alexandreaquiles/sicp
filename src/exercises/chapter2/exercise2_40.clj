fcc6dea4a8605cffc2df64902c82768b3c9a0251
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-10-11 18:57:36 -0300

    Exercise 2.40

---
+++ src/exercises/chapter2/exercise2_40.clj
@@ -0,0 +1,43 @@
+(ns exercises.chapter2.exercise2_40
+  (:use exercises.math))
+
+; Exercise 2.40:
+; Define a procedure unique-pairs that, given an integer n,
+;   generates the sequence of pairs (i, j) with 1 ≤ j < i ≤ n.
+; Use unique-pairs to simplify the definition of prime-sum-pairs
+;   given above.
+
+(def append concat)
+
+(def accumulate reduce)
+
+(defn enumerate-interval [low high]
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (inc low) high))))
+
+(defn flatmap [proc seq]
+  (accumulate append nil (map proc seq)))
+
+(defn unique-pairs [n]
+  (flatmap
+    (fn [i]
+      (map (fn [j] (list i j))
+           (enumerate-interval 1 (- i 1))))
+    (enumerate-interval 1 n)))
+
+(unique-pairs 6)
+; => ((2 1) (3 1) (3 2) (4 1) (4 2) (4 3) (5 1) (5 2) (5 3) (5 4) (6 1) (6 2) (6 3) (6 4) (6 5))
+
+(defn prime-sum? [pair]
+  (prime? (+ (first pair) (first (rest pair)))))
+
+(defn make-pair-sum [pair]
+  (list (first pair) (first (rest pair)) (+ (first pair) (first (rest pair)))))
+
+(defn prime-sum-pairs [n]
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs 6)
+; => ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7) (6 1 7) (6 5 11))
