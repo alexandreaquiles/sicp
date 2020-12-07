5d76eaa35e855a058fc499bb7fef51c4e1bae9b5
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-12-07 18:11:43 -0300

    Representing sets as unordered lists

--- src/exercises/chapter2/text.clj
+++ src/exercises/chapter2/text.clj
@@ -725,3 +725,42 @@ one-through-four
 
 (deriv '(* (* x y) (+ x 3)) 'x)
 ; => (+ (* x y) (* y (+ x 3)))
+
+(defn element-of-set? [x set]
+  (cond (empty? set) false
+        (= x (first set)) true
+        :else (element-of-set? x (rest set))))
+
+(element-of-set? 3 '(1 2 3 4))
+; => true
+
+(element-of-set? 9 '(1 2 3 4))
+; => false
+
+(element-of-set? 'y '(x y z w))
+; => true
+
+(element-of-set? 'k '(x y z w))
+; => false
+
+(defn adjoin-set [x set]
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(adjoin-set 1 '())
+; => (1)
+
+(adjoin-set 1 '(1))
+; => (1)
+
+(defn intersection-set [set1 set2]
+  (cond (or (empty? set1) (empty? set2)) '()
+        (element-of-set? (first set1) set2) (cons (first set1) (intersection-set (rest set1) set2))
+        :else (intersection-set (rest set1) set2)))
+
+(intersection-set '(1 2) '(3 4))
+; => ()
+
+(intersection-set '(1 2) '(2 3))
+; => (2)
