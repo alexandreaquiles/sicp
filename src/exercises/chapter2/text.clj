5d9d8147d877ff0940bf49f8a811e71738c722ec
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-03-17 20:32:28 -0300

    representing huffman trees, decoding, set of symbols-weight pairs ordered by weight

--- src/exercises/chapter2/text.clj
+++ src/exercises/chapter2/text.clj
@@ -1097,3 +1097,65 @@ one-through-four
 
 (lookup "Car" records)
 ; => false
+
+(defn make-leaf [symbol weight] (list 'leaf symbol weight))
+(defn leaf? [object] (= (first object) 'leaf))
+(defn symbol-leaf [x] (second x))
+(defn weight-leaf [x] (nth x 2))
+
+(defn left-branch [tree] (first tree))
+(defn right-branch [tree] (second tree))
+(defn symbols [tree]
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (nth tree 2)))
+(defn weight [tree]
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (nth tree 3)))
+
+(defn make-code-tree [left right]
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(make-code-tree
+  (make-leaf 'A 8)
+  (make-leaf 'B 9))
+; => ((leaf A 8) (leaf B 9) (A B) 17)
+
+(defn choose-branch [bit branch]
+  (cond (= bit 0) (left-branch branch)
+        (= bit 1) (right-branch branch)
+        :else (throw (Exception. "bad bit: CHOOSE-BRANCH"))))
+
+(defn decode [bits tree]
+  (defn decode-1 [bits current-branch]
+    (if (empty? bits)
+      '()
+      (let [next-branch (choose-branch (first bits) current-branch)]
+        (if (leaf? next-branch)
+          (cons (symbol-leaf next-branch)
+                (decode-1 (rest bits) tree))
+          (decode-1 (rest bits) next-branch)))))
+  (decode-1 bits tree))
+
+(decode '(0 1) '((leaf A 8) (leaf B 9) (A B) 17))
+; => (A B)
+
+(defn adjoin-set [x set]
+  (cond (empty? set) (list x)
+        (< (weight x) (weight (first set))) (cons x set)
+        :else (cons (first set) (adjoin-set x (rest set)))))
+
+(defn make-leaf-set [pairs]
+  (if (empty? pairs)
+    '()
+    (let [pair (first pairs)]
+      (adjoin-set (make-leaf (first pair)
+                             (second pair))
+                  (make-leaf-set (rest pairs))))))
+
+(make-leaf-set '((A 4) (B 2) (C 1) (D 1)))
+; => ((leaf D 1) (leaf C 1) (leaf B 2) (leaf A 4))
